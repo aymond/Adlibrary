dc0ef11d467a3bfcd0e26179b5748a994a2f9586
Aymon Delbridge <aymon.delbridge+github@googlemail.com>
2011-07-06 22:33:11 +0200

    added download book function

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 <?php 
 include 'sharedFunctions.php';
 include 'libraryConfig.php';
+include 'bookProcess.php';
 $files = read_folder_directory($library_uploadDir);
 
-?><table><tr><td>Link</td><td>File Name</td><td>Author</td><td>Series</td><td>Title</td></tr><?php
 if ($files) 
 { 
 	// $files is an array of filenames
@@ -18,30 +18,22 @@ if ($files)
 		
 		if ($tokens[2]) {
 			// Finds the last . in the name, and copies the string until this character
-			$bookTitle = substr($tokens[2], 0, strrpos($tokens[2], '.'));
-			$bookSeries = $tokens[1];
+			$bookTitle = trim(substr($tokens[2], 0, strrpos($tokens[2], '.')));
+			$bookSeries = trim($tokens[1]);
 			
-			$bookAuthor = $tokens[0];
+			$bookAuthor = trim($tokens[0]);
 		} elseif ($tokens[1]) {
 			$bookSeries = '';
-			$bookTitle = substr($tokens[1], 0, strrpos($tokens[1], '.'));
-			$bookAuthor = $tokens[0];
+			$bookTitle = trim(substr($tokens[1], 0, strrpos($tokens[1], '.')));;
+			$bookAuthor = trim($tokens[0]);
 		} else {
 			$bookSeries = '';
 			$bookTitle = '';
 			$bookAuthor = '';
-			$bookTitle = substr($tokens[0], 0, strrpos($tokens[0], '.'));
+			$bookTitle = trim(substr($tokens[0], 0, strrpos($tokens[0], '.')));
 		}
-	        ?>
-		<form action=action.php" method="post">		
-		<tr <?php if ($row==''){echo "class=\"D0\"";$row=1;}else{echo"class=\"D1\"";$row='';}?>>
-		<td><?php $fileURL="library_upload/".rawurlencode($file);?><a href=<?php echo $fileURL; ?> ><img src='/images/Files-Download-File-icon48x48.png'></a></td>
-		<td><input type="text" size="90" name="fileName" value="<?php echo trim($file); ?>" /></td>
-		<td><input type="text" size="25" name="bookAuthor" value="<?php echo trim($bookAuthor); ?>" /></td>
-		<td><input type="text" size="35" name="bookSeries" value="<?php echo trim($bookSeries); ?>" /></td>
-		<td><input type="text" size="50" name="bookTitle" value="<?php echo trim($bookTitle); ?>" /></td>
-		<td><img src='/images/Misc-Upload-Database-icon48x48.png'></td>
-		</tr></form><?php
+	        display_new_book_in_form('action.php', $file, $bookTitle, $bookSeries, $bookAuthor, $bookTitle);
+		
 	} 
 } else {
 	echo "No files available";

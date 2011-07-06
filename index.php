6a9db0d66b30a883797030872a65faed2c089ea3
Aymon Delbridge <aymon.delbridge+github@googlemail.com>
2011-07-06 20:32:02 +0200

    added tables and formatting

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<html><head><title>ADLibrary</title></head>
+<html><head><title>ADLibrary</title>
+<link rel="stylesheet" href="css/library.css" media="all"></head>
 <body>
 <b>List of files that have not been processed.</b><br><hr>
 <?php 
@@ -6,6 +7,7 @@ include 'sharedFunctions.php';
 include 'libraryConfig.php';
 $files = read_folder_directory($library_uploadDir);
 
+?><table><tr><td>Link</td><td>File Name</td><td>Author</td><td>Series</td><td>Title</td></tr><?php
 if ($files) 
 { 
 	// $files is an array of filenames
@@ -30,15 +32,23 @@ if ($files)
 			$bookAuthor = '';
 			$bookTitle = substr($tokens[0], 0, strrpos($tokens[0], '.'));
 		}
-
-		echo "Author: ".$bookAuthor."<br />";
-		echo "Series: ".$bookSeries."<br />";
-		echo "Title : ".$bookTitle."<br />";
-		echo "File  : ".$file."<br /><hr>";
-	
+	        ?>
+		<form action=action.php" method="post">		
+		<tr <?php if ($row==''){echo "class=\"D0\"";$row=1;}else{echo"class=\"D1\"";$row='';}?>>
+		<td><?php $fileURL="library_upload/".rawurlencode($file);?><a href=<?php echo $fileURL; ?> ><img src='/images/Files-Download-File-icon48x48.png'></a></td>
+		<td><input type="text" size="90" name="fileName" value="<?php echo trim($file); ?>" /></td>
+		<td><input type="text" size="25" name="bookAuthor" value="<?php echo trim($bookAuthor); ?>" /></td>
+		<td><input type="text" size="35" name="bookSeries" value="<?php echo trim($bookSeries); ?>" /></td>
+		<td><input type="text" size="50" name="bookTitle" value="<?php echo trim($bookTitle); ?>" /></td>
+		<td><img src='/images/Misc-Upload-Database-icon48x48.png'></td>
+		</tr></form><?php
 	} 
-} 
+} else {
+	echo "No files available";
+}
 ?>
+</table>
+<label>
 <form action="action.php" method="post">
 <p>Book Title: <input type="text" name="title" /> </p>
 <p>Book Author: <input type="text" name="author" /> </p>
@@ -47,5 +57,6 @@ if ($files)
 
 <p><input type="submit" /></p>
 </form>
+</label>
 </body>
 </html>
